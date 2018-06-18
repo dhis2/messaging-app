dbf5647ae6de5f45b61daf6f665289dda6825477
Henning Håkonsen <henninghaakonsen@me.com>
2018-06-18 11:54:01 +0200

    Ejected from CRA, fixed dependency issues and added one test.

--- app/src/reducers/__tests__/messageConversations.spec.js
+++ app/src/reducers/__tests__/messageConversations.spec.js
@@ -3,7 +3,7 @@ import actions from '../../constants/actions';
 
 describe('message conversation reducer', () => {
     it('should return the default state', () => {
-        const actualState = reducer(undefined, {});
+        const actualState = messageReducer(undefined, {});
 
         expect(actualState).toEqual(initialState);
     });
