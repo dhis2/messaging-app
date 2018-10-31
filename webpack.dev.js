813655594cb6d09905821da913f93c8e621ca34b
Hendrik de Graaf <hendrik@dhis2.org>
2018-10-31 10:22:07 +0100

    Load shared CSS correctly in dev

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,8 @@
 const merge = require('webpack-merge')
 const webpack = require('webpack')
 const common = require('./webpack.common.js')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const parse = require('url-parse');
 
 const dhisConfigPath =
     process.env.DHIS2_HOME && `${process.env.DHIS2_HOME}/config`
@@ -21,18 +23,34 @@ const globals = {
     DHIS_CONFIG: JSON.stringify(dhisConfig),
 }
 
+const pathnamePrefix = parse(dhisConfig.baseUrl).pathname;
+
 module.exports = merge(common, {
     output: {
         publicPath: '/',
     },
     plugins: [
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: 'public/index.html',
+            vendorScripts: [
+                `.${pathnamePrefix}/dhis-web-core-resource/material-design-icons/material-icons.css`,
+                `.${pathnamePrefix}/dhis-web-core-resource/fonts/roboto.css`,
+            ]
+                .map(asset => {
+                    return /\.js$/.test(asset)
+                        ? `<script src="${asset}"></script>`
+                        : `<link type="text/css" rel="stylesheet" href="${asset}">`;
+                })
+                .join('\n'),
+        }),
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify('development'),
         }),
         new webpack.DefinePlugin(globals),
     ],
     devServer: {
-        port: 9000,
+        port: 3000,
         inline: true,
         contentBase: './app',
         historyApiFallback: true,
