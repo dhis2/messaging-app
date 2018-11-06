5ca3208ebf4f11f502f6e3714e38297aa7265358
Hendrik de Graaf <hendrik@dhis2.org>
2018-11-06 11:35:14 +0100

    Introduced redux-thunk

--- app/src/store.js
+++ app/src/store.js
@@ -1,11 +1,12 @@
 import { createStore, applyMiddleware, combineReducers, compose } from 'redux'
 import { createEpicMiddleware } from 'redux-observable'
+import ReduxThunk from 'redux-thunk'
 import { createLogger } from 'redux-logger'
 
 import messageReducer from 'reducers/messageReducer'
 import Epics from 'actions/epics'
 
-const middlewares = [createEpicMiddleware(Epics)]
+const middlewares = [createEpicMiddleware(Epics), ReduxThunk]
 
 // eslint-disable-next-line
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
