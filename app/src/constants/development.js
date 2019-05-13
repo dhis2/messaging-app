442037eca109666e3382eb0e3cec656bdcb5a194
ismay <ismay@ismaywolff.nl>
2019-05-13 16:52:46 +0200

    chore: reset to default CRA

--- app/src/constants/development.js
+++
@@ -1,6 +0,0 @@
-export const pageSize = 35
-export const fontFamily = 'Roboto, Helvetica, Arial, sans-serif'
-
-export const NEUTRAL = 'NEUTRAL'
-export const POSITIVE = 'POSITIVE'
-export const NEGATIVE = 'NEGATIVE'
