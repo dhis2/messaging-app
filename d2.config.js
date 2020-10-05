924663a71c0355e903a46ec2859f031b0f17ae2b
Hendrik de Graaf <hendrik@dhis2.org>
2020-10-05 15:04:45 +0200

    chore: initialised the project with cli-app-scripts

---
+++ d2.config.js
@@ -0,0 +1,12 @@
+const config = {
+    type: 'app',
+    coreApp: true,
+    title: 'Messaging',
+    name: 'messaging',
+    description: 'An application for sending internal messages',
+    entryPoints: {
+        app: './src/components/AppWrapper',
+    },
+}
+
+module.exports = config
