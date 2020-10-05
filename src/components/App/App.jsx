8ecc9e3dd2432988754e5865b4d5346386bcd104
Hendrik de Graaf <hendrik@dhis2.org>
2020-10-05 16:03:37 +0200

    chore: remove CssReset as it is included in the app-shell

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import { CssReset } from '@dhis2/ui-core'
 
 import CustomSnackBar from '../Common/CustomSnackBar'
 import Routes from './Routes'
 
 const App = () => (
     <div>
-        <CssReset />
         <CustomSnackBar />
         <Routes />
     </div>
